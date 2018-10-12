10a01aad0086a0fd99ed158a01811f16a8fe0d33
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-12 15:07:03 +0800

    add: Tips in router

--- generator/template/src/router/index.js
+++ generator/template/src/router/index.js
@@ -25,6 +25,14 @@ const Delete = {
   template: '<div class="title">Delete</div>',
 };
 
+/**
+ * TIPS:
+ * meta: {
+ *   hidden: false,   // If `hidden:true` will not appear in the navigation bar or sidebar(default is false)
+ *   auth: [],        // It will control the page roles (you can set multiple roles)
+ *   icon: 'home',    // Icon will appear in the navigation bar or sidebar
+ * }
+ */
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
