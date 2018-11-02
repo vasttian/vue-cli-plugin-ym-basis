6ee975d15cad9fe20b3657fc9b8b6805c796d4ae
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-11-03 01:06:48 +0800

    mod: layout

--- generator/template/src/views/layouts/components/AppMain.vue
+++ generator/template/src/views/layouts/components/AppMain.vue
@@ -1,23 +1,21 @@
 <template>
-  <section>
-    <%_ if (ui === 'element') { _%>
-    <el-main>
-      <transition name="move" mode="out-in">
+  <%_ if (ui === 'element') { _%>
+  <el-main>
+    <transition name="move" mode="out-in">
+      <router-view/>
+    </transition>
+  </el-main>
+  <%_ } else if (ui === 'vuetify') { _%>
+  <v-content>
+    <v-container fluid>
+      <v-slide-y-transition mode="out-in">
         <router-view/>
-      </transition>
-    </el-main>
-    <%_ } else if (ui === 'vuetify') { _%>
-    <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <router-view/>
-        </v-slide-y-transition>
-      </v-container>
-    </v-content>
-    <%_ } else { _%>
-    <router-view/>
-    <%_ } _%>
-  </section>
+      </v-slide-y-transition>
+    </v-container>
+  </v-content>
+  <%_ } else { _%>
+  <router-view/>
+  <%_ } _%>
 </template>
 
 <script>
